9e699a9006f92e5ab415322d3003214cb2f679e8
pauloemil <prog.paulo@hotmail.com>
2022-05-15 12:24:52 +0200

    navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import Header from "../components/HomeScreenComponents/Header";
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Header />
+      <Header navigation={navigation} />
       <ConversationList navigation={navigation} style={styles.list} />
       <FloatingStartingButton
         navigation={navigation}
