8a7c4faa222ba2c7344c2c73d6a9cb374308c777
pauloemil <prog.paulo@hotmail.com>
2022-05-16 10:28:57 +0200

    Refactor code, Add logout

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import ImagePickerComponent from "../components/ProfileScreenComponents/ImagePickerComponent";
 import InformationTexts from "../components/ProfileScreenComponents/InformationTexts";
+import Icon from "react-native-vector-icons/Entypo";
 
-const ProfileScreen = () => {
+import { removeValue } from "../configs/asyncStorageHelper";
+
+const ProfileScreen = ({ route }) => {
   const [firstName, setfirstName] = useState("Paulo");
   const [secondName, setSecondName] = useState("Emil");
   const [mobileNumber, setMobileNumber] = useState("+201066122321");
   const [address, setAddress] = useState("Tur-Sinai 5 st. new Kelany");
+  const handlePress = () => {
+    removeValue("accessToken");
+    removeValue("refreshToken");
+    route.params.setIsLogged(false);
+  };
   return (
     <KeyboardAwareScrollView>
       <View style={styles.container}>
@@ -45,6 +59,12 @@ const ProfileScreen = () => {
             placeholder="Address"
           />
         </View>
+        <TouchableOpacity onPress={() => handlePress()}>
+          <View style={styles.logoutButton}>
+            <Icon name="log-out" size={24} color="black" />
+            <Text style={styles.buttonText}>Logout</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </KeyboardAwareScrollView>
   );
@@ -70,6 +90,19 @@ const styles = StyleSheet.create({
   },
   inputText: {
     marginTop: 20,
+  },
+  logoutButton: {
+    backgroundColor: "coral",
+    width: 150,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    marginVertical: 20,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
 export default ProfileScreen;
