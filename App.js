f0f889ed9aec01a974dad9dd4acfff9404d9fc05
pauloemil <prog.paulo@hotmail.com>
2022-05-16 10:25:28 +0200

    add auth and remove unnecessary code

--- App.js
+++ App.js
@@ -10,23 +10,47 @@ enableScreens();
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { Store } from "./src/redux/store";
 import SignInScreen from "./src/screens/SignInScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
 import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
 import OTPScreen from "./src/screens/OTPScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import SignUpInfoScreen from "./src/screens/SignUpInfoScreen";
+import { useState, useEffect } from "react";
+import LoadingScreen from "./src/screens/LoadingScreen";
+import {
+  checkAuth,
+  getAllKeys,
+  removeValue,
+} from "./src/configs/asyncStorageHelper";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const isLogged = true;
+  const [isLogged, setIsLogged] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // getAllKeys();
+  checkAuth(setIsLoading, setIsLogged);
   return (
     <Provider store={Store}>
       <NavigationContainer>
         <Stack.Navigator>
           {!isLogged ? (
             <>
+              <Stack.Screen
+                name="SignIn"
+                options={{ headerShown: false }}
+                component={SignInScreen}
+                initialParams={{ setIsLogged: setIsLogged }}
+              />
+              <Stack.Screen
+                name="SignUp"
+                options={{ headerShown: false }}
+                component={SignUpScreen}
+              />
               <Stack.Screen
                 name="ForgotPassword"
                 options={{ headerShown: false }}
@@ -37,16 +61,7 @@ export default function App() {
                 options={{ headerShown: false }}
                 component={OTPScreen}
               />
-              <Stack.Screen
-                name="SignIn"
-                options={{ headerShown: false }}
-                component={SignInScreen}
-              />
-              <Stack.Screen
-                name="SignUp"
-                options={{ headerShown: false }}
-                component={SignUpScreen}
-              />
+
               <Stack.Screen
                 name="ResetPassword"
                 options={{ headerShown: false }}
@@ -59,6 +74,12 @@ export default function App() {
                 name="Home"
                 options={{ headerShown: false }}
                 component={HomeScreen}
+              />
+              <Stack.Screen
+                name="Profile"
+                // options={{ headerShown: false }}
+                component={ProfileScreen}
+                initialParams={{ setIsLogged: setIsLogged }}
               />
               <Stack.Screen
                 options={{ headerShown: false }}
@@ -90,3 +111,22 @@ export default function App() {
 // const HomeStack = createStackNavigator(screens);
 
 // export default createAppContainer(HomeStack);
+{
+  /* <Stack.Screen
+            name="Loading"
+            options={{ headerShown: false }}
+            component={LoadingScreen}
+          /> */
+}
+{
+  /* <Stack.Screen
+                name="MoreInfo"
+                options={{ headerShown: false }}
+                component={SignUpInfoScreen}
+              /> */
+}
+<Stack.Screen
+  name="Loading"
+  options={{ headerShown: false }}
+  component={LoadingScreen}
+/>;
